621128aeee1a2d901ce7bc7478d0d7f12cdaeaf0
mundoflavinha <dev@mundoflavinha.com>
2025-07-08 01:25:28 -0300

    chore: Moving Promo Banner

--- src/pages/RodaDeConversaEmFamilia/Components/PromoBannerTop.tsx
+++ src/pages/RodaDeConversaEmFamilia/Components/PromoBannerTop.tsx
@@ -3,9 +3,9 @@ const PromoBannerTop = () => {
     // <div className="w-full bg-[#45a29e] text-white text-center py-3 px-2 font-bold text-base md:text-lg shadow-md animate-pulse-slow flex flex-col md:flex-row items-center justify-center gap-2">
     <div className="w-full bg-quinary text-white text-center py-3 px-2 font-bold text-base md:text-lg shadow-md animate-pulse-slow flex flex-col md:flex-row items-center justify-center gap-2">
       <span>
-        Atenção: Últimos dias de promoção! <span className="underline">Ganhe um bônus especial de presente.</span>
+        Atenção: Últimos dias de promoção!   <br className="block md:hidden" /><span className="underline">Ganhe um bônus especial de presente.</span>
       </span>
-      <a href="#" className="inline-block bg-white text-pink-600 font-semibold rounded-full px-5 py-2 ml-0 md:ml-4 shadow hover:bg-yellow-100 transition-colors duration-300">
+      <a href="#limited-offer" className="inline-block bg-white text-pink-600 font-semibold rounded-full px-5 py-2 ml-0 md:ml-4 shadow hover:bg-yellow-100 transition-colors duration-300">
         Aproveite agora!
       </a>
     </div>
