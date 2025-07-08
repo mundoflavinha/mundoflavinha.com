4149c62fc786c1b0e25cecc7008e0b8aaa27fff2
mundoflavinha <dev@mundoflavinha.com>
2025-07-08 01:26:08 -0300

    chore: Temp redirect

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./pages/Index";
+import { Navigate } from "react-router-dom";
 import Roda from "./pages/RodaDeConversaEmFamilia/Index";
 
 export const routes = [
   {
     path: "/",
-    element: <Home />,
+    // element: <Home />,
+    element: <Navigate to="/roda-de-conversa-em-familia" replace />,
     index: true,
   },
   {
