a22d5ba03fba8288f640b68204bcc5e480a17d4c
mundoflavinha <dev@mundoflavinha.com>
2025-07-24 11:58:57 -0300

    fix: replace "jogo" for "kit".

--- src/pages/DiaDosPais/Components/OfferSection.tsx
+++ src/pages/DiaDosPais/Components/OfferSection.tsx
@@ -21,7 +21,7 @@ const OfferSection = () => {
                 onClick={() => window.open('https://pay.cakto.com.br/zk8k9os_475370', '_blank')}  
                 className="group px-8 py-6 bg-primary text-lg hover:bg-opacity-90 text-white rounded-full font-medium hover:bg-secondary transform transition-all duration-300 hover:scale-105 shadow-lg hover:shadow-xl animate-pulse-glow"
               >
-                Quero garantir o meu jogo agora!
+                Quero garantir o meu kit agora!
               </Button>
             </div>
           </div>
